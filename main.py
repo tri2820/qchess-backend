c106e5a259c71f1b289505a23e830c88828a2814
Tri Nguyen Van <nguyenvantri2820@gmail.com>
2025-01-13 00:53:00 +0100

    feat

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from typing import Optional
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from qiskit import QuantumCircuit
 from qiskit.primitives import Sampler
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
